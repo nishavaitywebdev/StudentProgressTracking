f1c31d0aa0eace378113d4ae1d1babf78609e6a1
Nisha Vaity <nishavaity@Nishas-MacBook-Pro.local>
2017-09-27 14:37:11 -0400

    Next to next

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session       = require('express-session');
+
+const passport = require('passport');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+app.listen(app.get('port'));
+
+
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen();
